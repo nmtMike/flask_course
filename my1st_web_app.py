a231f42f9f1c4ffe46753804539401d3a15064e2
nmtMike <108139735+nmtMike@users.noreply.github.com>
2022-09-24 20:54:53 +0700

    add some pages for my1st_web_app

--- my1st_web_app.py
+++ my1st_web_app.py
@@ -6,5 +6,17 @@ my_web_app = Flask(__name__)
 def index():
     return '<h1>Hello there, this is my first web app using Flask !!!</h1>'
 
+@my_web_app.route('/information')
+def infomation():
+    return '''<h1>This web app is based on:</h1>
+    Python: 3.9.12 and Flask: 1.1.2
+    '''
+
+@my_web_app.route('/user/<name>')
+def user(name):
+    return """
+    This page is for {}
+    """.format(name.upper())
+
 if __name__ == '__main__':
     my_web_app.run()
