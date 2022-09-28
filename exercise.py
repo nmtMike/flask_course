ef575eb957b4f73e78b7c76993de8122aaef2b8c
nmtMike <108139735+nmtMike@users.noreply.github.com>
2022-09-28 12:33:12 +0700

    modified:   exercise.py 	modified:   templates/base.html 	modified:   templates/home_page.html 	new file:   templates/result_right.html 	new file:   templates/result_wrong.html

--- exercise.py
+++ exercise.py
@@ -4,12 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
-
-@app.route('/home_page')
-def home_page():
     return render_template('home_page.html')
 
+@app.route('/<UserName>')
+def user_check(UserName):
+    requirements = []
+    if not UserName[-1].isnumeric():
+        requirements += ['Must have a numeric at the end']
+    if UserName.isupper():
+        requirements += ['At least a lower case letter']
+    if UserName.islower():
+        requirements += ['At least an upper case letter']
+
+    if len(requirements) == 0:
+        return render_template('result_right.html', UserName=UserName)
+    else:
+        return render_template('result_wrong.html',UserName=UserName, requirements=requirements)
 
 if __name__ == '__main__':
     app.run()
